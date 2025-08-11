af76548f42e7f62a8ba0f4417d1fb1199264a6b9
CoderManChild <atharvak2025@gmail.com>
2025-08-11 12:49:29 -0400

    Removed test folder

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import List
+from supabase import create_client
+import uuid
 import os
 
 app = FastAPI()
+
+#app.mount("/uploaded_photos", StaticFiles(directory="app/uploaded_photos"), name="uploaded_photos")
+
+SUPABASE_URL = "https://itshkkqzqobiorqakeyk.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0c2hra3F6cW9iaW9ycWFrZXlrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDkyMDE0OCwiZXhwIjoyMDcwNDk2MTQ4fQ.449KwAysQCJUVSL3iq_5ec35SOsiJFLk6AaKlyxC_tQ"
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 class Photo(BaseModel):
     title: str
@@ -12,6 +22,7 @@ class Photo(BaseModel):
     longitude: float
     id: int
     image_filename: str
+    url: str
 
 photos_db: List[Photo] = []
 @app.get("/")
@@ -28,20 +39,25 @@ async def add_photo(
     longitude: float = Form(),
     file: UploadFile = File()
 ):
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
-    upload_folder = os.path.join(BASE_DIR, "uploaded_photos")
-    file_path = os.path.join(upload_folder, file.filename)
+    file_bytes = await file.read()
+    file_ext = file.filename.split(".")[-1]
+    unique_filename = f"{uuid.uuid4()}.{file_ext}"
+    upload_response = supabase.storage.from_("photos").upload(unique_filename, file_bytes)
+    public_url = supabase.storage.from_("photos").get_public_url(unique_filename)
+
+    insert_response = supabase.table("photos").insert({
+        "title": title,
+        "description": description,
+        "latitude": latitude,
+        "longitude": longitude,
+        "image_url": public_url
+    }).execute()
+
     
-    contents = await file.read()
-    with open(file_path, "wb") as f:
-        f.write(contents)
-    
-    new_id = len(photos_db) + 1
-    new_photo = Photo(title=title, description=description, latitude=latitude, longitude=longitude, id=new_id, image_filename=file.filename)
-    photos_db.append(new_photo)
-    return {"message": "Photo info received!", "photo": new_photo}
+    return {"message": "Photo uploaded successfully!", "photo": insert_response.data[0]}
+
 
 @app.get('/photos/', response_model=List[Photo])
-async def list_photos():
+async def get_photos():
     return photos_db
 
