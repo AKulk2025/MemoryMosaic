edcdfe4eab650d53501b93fda75df1b7b642a8eb
AKulk2025 <atharvak2025@gmail.com>
2026-01-13 21:14:53 -0500

    Spring Boot initialization

---
+++ src/main/java/memory_mosaic/example/memory/mosaic/MemoryMosaicApplication.java
@@ -0,0 +1,13 @@
+package memory_mosaic.example.memory.mosaic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryMosaicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryMosaicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/memory_mosaic/example/memory/mosaic/MemoryMosaicApplicationTests.java
@@ -0,0 +1,13 @@
+package memory_mosaic.example.memory.mosaic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryMosaicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
